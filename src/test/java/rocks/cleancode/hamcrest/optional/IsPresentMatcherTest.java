af82bbabc52b94a133d71a407c5ddd3d5d0a1444
Benjamin PREVOT <me@benjaminprevot.fr>
2022-09-05 22:46:46 +0200

    feat: Matcher for optional with value

---
+++ src/test/java/rocks/cleancode/hamcrest/optional/IsPresentMatcherTest.java
@@ -0,0 +1,19 @@
+package rocks.cleancode.hamcrest.optional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class IsPresentMatcherTest {
+
+    @Test
+    public void should_match_optional_with_value() {
+        Optional<String> optionalWithValue = Optional.of("Dummy value");
+
+        assertThat(optionalWithValue, is(new IsPresentMatcher()));
+    }
+
+}
