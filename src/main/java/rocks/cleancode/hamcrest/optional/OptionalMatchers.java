95b7e8691b2efea59f3e00f16555c148cebcb818
Benjamin PREVOT <benjaminprevot@users.noreply.github.com>
2022-11-01 07:14:10 +0100

    feat: Facility class to create matchers
    Github-PR: #55

---
+++ src/main/java/rocks/cleancode/hamcrest/optional/OptionalMatchers.java
@@ -0,0 +1,54 @@
+package rocks.cleancode.hamcrest.optional;
+
+import org.hamcrest.Matcher;
+
+import java.util.Optional;
+
+/**
+ * Facility class to create matchers for {@link java.util.Optional}.
+ *
+ * @since 1.1.0
+ */
+public class OptionalMatchers {
+
+    /**
+     * Create a matcher for empty {@link java.util.Optional}.
+     *
+     * @return Empty {@link java.util.Optional} matcher
+     * @param <T> Type of the {@link java.util.Optional} value
+     *
+     * @since 1.1.0
+     */
+    public static <T> Matcher<Optional<T>> empty() {
+        return IsEmptyMatcher.empty();
+    }
+
+    /**
+     * Create a matcher for {@link java.util.Optional} having a value.
+     *
+     * @return Present {@link java.util.Optional} matcher
+     * @param <T> Type of the {@link java.util.Optional} value
+     *
+     * @since 1.1.0
+     */
+    public static <T> Matcher<Optional<T>> present() {
+        return IsPresentMatcher.present();
+    }
+
+    /**
+     * Create a matcher for the value of the {@link java.util.Optional}.
+     *
+     * @param valueMatcher Matcher for the value
+     * @return Value matcher
+     * @param <T> Type of the {@link java.util.Optional} value
+     *
+     * @since 1.1.0
+     */
+    public static <T> Matcher<Optional<T>> value(Matcher<T> valueMatcher) {
+        return ValueMatcher.value(valueMatcher);
+    }
+
+    private OptionalMatchers() {
+    }
+
+}
---
+++ src/test/java/rocks/cleancode/hamcrest/optional/OptionalMatchersTest.java
@@ -0,0 +1,36 @@
+package rocks.cleancode.hamcrest.optional;
+
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+
+public class OptionalMatchersTest {
+
+    @Test
+    public void should_create_is_empty_matcher() {
+        Matcher<Optional<String>> isEmptyMatcher = OptionalMatchers.empty();
+
+        assertThat(isEmptyMatcher, is(instanceOf(IsEmptyMatcher.class)));
+    }
+
+    @Test
+    public void should_create_is_present_matcher() {
+        Matcher<Optional<String>> isPresentMatcher = OptionalMatchers.present();
+
+        assertThat(isPresentMatcher, is(instanceOf(IsPresentMatcher.class)));
+    }
+
+    @Test
+    public void should_create_value_matcher() {
+        Matcher<Optional<String>> valueMatcher = OptionalMatchers.value(is(equalTo("Dummy value")));
+
+        assertThat(valueMatcher, is(instanceOf(ValueMatcher.class)));
+    }
+
+}
